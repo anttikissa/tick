cfc06aa3000b70976c3b04df22ea7acc2ae28594
Antti Sykäri <antti.sykari@gmail.com>
2013-01-19 12:12:18 +0200

    Semi-initial.

---
+++ index.js
@@ -0,0 +1,18 @@
+var log = require('./my-log');
+
+function tick() {
+
+}
+
+function start() {
+	console
+}
+
+function end() {
+}
+
+tick.start = start;
+tick.end = end;
+
+module.exports = tick;
+
