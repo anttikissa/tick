8e1e2fea84c886d8bf949757707fd246f5ad2b3a
Antti Sykäri <antti.sykari@gmail.com>
2013-01-23 10:11:18 +0200

    Sketch some more.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var log = require('./my-log');
+var log = require('basic-log');
 
 function tick() {
-
+	var args = Array.prototype.join.call(arguments);
+	log('tick ' + args);
 }
 
 function start() {
-	console
+	log('start');
 }
 
 function end() {
+	log('end');
 }
 
 tick.start = start;
